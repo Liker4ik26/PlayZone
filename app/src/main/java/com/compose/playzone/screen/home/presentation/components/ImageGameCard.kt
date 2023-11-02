9626cdbb75f7b63d689967edc2695595ccee8510
Liker4ik50 <likakan85@gmail.com>
2023-11-03 01:52:55 +0600

    [+] Added play button

--- app/src/main/java/com/compose/playzone/screen/home/presentation/components/ImageGameCard.kt
+++ app/src/main/java/com/compose/playzone/screen/home/presentation/components/ImageGameCard.kt
@@ -1,10 +1,15 @@
 package com.compose.playzone.screen.home.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,11 +19,19 @@ import com.compose.playzone.R
 @Composable
 fun ImageGameCard(image: Int) {
     Card(modifier = Modifier.height(128.dp)) {
-        Image(
-            painter = painterResource(id = image),
-            contentDescription = "null",
-            contentScale = ContentScale.Fit
-        )
+        Box {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = "null",
+                contentScale = ContentScale.Fit
+            )
+            PlayButton(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(48.dp)
+                    .clip(CircleShape),
+            )
+        }
     }
 }
 
