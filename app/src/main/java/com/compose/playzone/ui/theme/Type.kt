b2c2df06de6bfc6fbf7133e2a3b40edfaf50f5ff
Liker4ik50 <likakan85@gmail.com>
2023-11-02 18:04:09 +0600

    [+] Added a printing house

--- app/src/main/java/com/compose/playzone/ui/theme/Type.kt
+++ app/src/main/java/com/compose/playzone/ui/theme/Type.kt
@@ -2,33 +2,64 @@ package com.compose.playzone.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.compose.playzone.R
 
-// Set of Material typography styles to start with
+val SkModernist = FontFamily(
+    Font(R.font.sk_modernist_regular, FontWeight.Normal),
+    Font(R.font.sk_modernist_bold, FontWeight.Bold),
+    Font(R.font.sk_modernist_mono, FontWeight.Medium),
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat, FontWeight.Medium),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontFamily = SkModernist,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
+        lineHeight = 57.6.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    titleMedium = TextStyle(
+        fontFamily = SkModernist,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = SkModernist,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 19.2.sp,
+        letterSpacing = 0.6.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SkModernist,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 19.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = SkModernist,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 14.4.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        lineHeight = 12.19.sp,
+        letterSpacing = 0.5.sp
+    ),
 )
